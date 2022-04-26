92f5ba6e49a67d8ffdb22d0bba97e076781c02ef
lonkaars <loek@pipeframe.xyz>
2022-04-26 14:02:15 +0200

    fix bootstrap script

--- scripts/bootstrap.ps1
+++ scripts/bootstrap.ps1
@@ -1,7 +1,7 @@
 Start-BitsTransfer -Source https://github.com/msys2/msys2-installer/releases/download/2022-03-19/msys2-x86_64-20220319.exe -Destination .\msys2-x86_64-latest.exe
 .\msys2-x86_64-latest.exe in --confirm-command --accept-messages --root C:/msys64
 Remove-Item .\msys2-x86_64-latest.exe
-C:/msys64/usr/bin/sh.exe -c 'curl -s https://raw.githubusercontent.com/lonkaars/wall-e2/master/scripts/install-mingw-packages.sh | sh'
-C:/msys64/usr/bin/sh.exe -c 'git clone https://github.com/lonkaars/wall-e2'
-C:/msys64/usr/bin/sh.exe -c '~/wall-e2/scripts/install-sdk.sh'
-C:/msys64/usr/bin/sh.exe -c 'cd ~/wall-e2 && explorer.exe .'
+C:/msys64/msys2_shell.cmd -defterm -no-start -mingw64 -shell sh -c 'curl -s https://raw.githubusercontent.com/lonkaars/wall-e2/master/scripts/install-mingw-packages.sh | sh'
+C:/msys64/msys2_shell.cmd -defterm -no-start -mingw64 -shell sh -c 'git clone https://github.com/lonkaars/wall-e2'
+C:/msys64/msys2_shell.cmd -defterm -no-start -mingw64 -shell sh -c '~/wall-e2/scripts/install-sdk.sh'
+C:/msys64/msys2_shell.cmd -defterm -no-start -mingw64 -shell sh -c 'cd ~/wall-e2 && explorer.exe .'
